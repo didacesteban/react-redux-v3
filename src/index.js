7e8311f761ca41f1024c8e16dcabab2f410caec2
Didac Esteban <didacestebanandres@Didacs-MacBook-Pro.local>
2017-07-03 23:25:10 +0200

    created example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import ReactDOM from "react-dom";
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import createLogger from 'redux-logger';
+import allReducers from './reducers';
+import App from './components/app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(allReducers);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
